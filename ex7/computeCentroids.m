c24e6985ca3068efdcf2074e52537da61350b1de
Zizheng <zlinai@connect.ust.hk>
2017-07-31 20:49:45 -0700

    Finish implementing image compression using K means and PCA

---
+++ ex7/computeCentroids.m
@@ -0,0 +1,37 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids =[];
+
+for i=1:K
+  temp_idx = (idx == i);
+  num_points = sum(temp_idx);
+  new_cent = (1/num_points)*(X' * temp_idx);
+  centroids = [centroids;new_cent'];
+endfor
+
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
---
+++ ex7/recoverData.m
@@ -0,0 +1,15 @@
+function X_rec = recoverData(Z, U, K)
+%RECOVERDATA Recovers an approximation of the original data when using the 
+%projected data
+%   X_rec = RECOVERDATA(Z, U, K) recovers an approximation the 
+%   original data that has been reduced to K dimensions. It returns the
+%   approximate reconstruction in X_rec.
+%
+
+% You need to return the following variables correctly.
+Ured = U(:,1:K);
+Ured =Ured';
+X_rec = Z*Ured;
+
+
+end
